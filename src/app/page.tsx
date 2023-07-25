57d45b9446a9f3229dca7fd9e1c67e64708a7215
gmirg <gofthet@gmail.com>
2023-07-25 18:04:36 +0200

    first level 4x4 done

---
+++ src/app/page.tsx
@@ -0,0 +1,89 @@
+"use client"
+import { useState, useEffect } from "react";
+import styles from "./page.module.css";
+const board = ["🤖", "👽", "👻", "🤡", "🐧", "🦚", "😄", "🚀"];
+
+export default function Home() {
+  const [boardData, setBoardData] = useState<string[]>([]);
+  const [flippedCards, setFlippedCards] = useState<number[]>([]);
+  const [matchedCards, setMatchedCards] = useState<number[]>([]);
+  const [moves, setMoves] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  useEffect(() => {
+    initialize();
+  }, []);
+  useEffect(() => {
+    if (matchedCards.length == 16) {
+      setGameOver(true);
+    }
+  }, [moves]);
+  const shuffle = () => {
+    const shuffledCards = [...board, ...board] // Since we need pair of data so we will use the static board values twice using the spread operator.
+      .sort(() => Math.random() - 0.5)
+      .map((v) => v);
+
+    setBoardData(shuffledCards);
+  };
+  const initialize = () => {
+    shuffle();
+    setGameOver(false);
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setMoves(0);
+  };
+  /*   This function counts how many cards have you flipped until now
+   */
+  const updateActiveCards = (i) => {
+    if (!flippedCards.includes(i)) {
+      if (flippedCards.length == 1) {
+        const firstIdx = flippedCards[0];
+        const secondIdx = i;
+        if (boardData[firstIdx] == boardData[secondIdx]) {
+          setMatchedCards((prev) => [...prev, firstIdx, secondIdx]);
+        }
+
+        setFlippedCards([...flippedCards, i]);
+      } else if (flippedCards.length == 2) {
+        setFlippedCards([i]);
+      } else {
+        setFlippedCards([...flippedCards, i]);
+      }
+
+      setMoves((v) => v + 1);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="menu">
+        <p>{`Moves - ${moves}`}</p>
+      </div>
+
+      <div className="board">
+        {boardData.map((data, i) => {
+          const flipped = flippedCards.includes(i) ? true : false;
+          const matched = matchedCards.includes(i) ? true : false;
+          return (
+            <div
+              onClick={() => {
+                updateActiveCards(i);
+              }}
+              key={i}
+              className={`card ${flipped || matched ? "active" : ""} ${
+                matched ? "matched" : ""
+              } ${gameOver ? "gameover" : ""}`}
+            >
+              <div className="card-front">{data}</div>
+              <div className="card-back"></div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="menu">
+        <p>{`GameOver - ${gameOver}`}</p>
+        <button onClick={() => initialize()} className="reset-btn">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
